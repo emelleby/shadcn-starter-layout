09425842491b965304a8690a1c1ac52aad8543bc
Eivind Melleby <emelleby@gmail.com>
2025-01-06 22:19:11 +0100

    fix the require for Node 22

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ['class'],
@@ -90,5 +92,6 @@ export default {
   		}
   	}
   },
-  plugins: [require('tailwindcss-animate')],
+  // plugins: [require('tailwindcss-animate')],
+  plugins: [tailwindcssAnimate],
 };
